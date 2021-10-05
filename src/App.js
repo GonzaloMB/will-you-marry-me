84beadd9f1f3df7a3cca69e5f22244d87b2bb6f7
Gonzalo Meana Balseiro <gonzalomeanabalseiro@gmail.com>
2021-10-05 11:38:55 +0200

    summary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Confetti from "react-confetti";
+class App extends React.Component {
+  move() {
+    const height = 400;
+    const width = 400;
+    const btn = document.getElementById("myDIV");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    btn.addEventListener("mouseover", () => {
+      let randY = Math.floor(Math.random() * height + 1);
+      let randX = Math.floor(Math.random() * width + 1);
+      btn.style.top = randY + "px";
+      btn.style.right = randX + "px";
+    });
+  }
+
+  popupShow() {
+    var popup = document.getElementById("myPopup");
+    var confetti = document.getElementById("confetti");
+    popup.classList.toggle("show");
+    confetti.classList.toggle("yes");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="merry-counter">
+          <img src="./ring.png" className="App-logo" />
+          <h1>Will you marry me ?</h1>
+          <div className="button-counter" id="tsparticles">
+            <button className="yes-btn popup btnParty" onClick={this.popupShow}>
+              Yes 😍
+              <span className="popuptext" id="myPopup">
+                YEY! I already knew your answer 💕
+              </span>
+            </button>
+            <button id="myDIV" className="no-btn" onMouseOver={this.move}>
+              No 😢
+            </button>
+          </div>
+        </div>
+        <Confetti id="confetti" className="confetti" />
+      </div>
+    );
+  }
 }
 
 export default App;
